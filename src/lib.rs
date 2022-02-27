543ec692e6946d78243567855c7521d34daf46cb
Lukas Jackowski <l.jackowski@agorate.de>
2022-02-28 00:11:11 +0100

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,54 @@
+//! This is a plugin implementing OSC for bevy using `rosc`.
+//!
+//!
+//!
+//! # Usage
+//!
+//! First you need to add the [dispatcher](bevy_osc::osc_dispatcher::OscDispatcher) as a resource to your app.
+//! Then add an [OscMethod](bevy_osc::osc_method::OscMethod) component to your entity.
+//! The dispatcher will now deliver all OSC messages that match the method's address to your component.
+//!
+//! ```
+//! use bevy::prelude::*;
+//! use bevy_osc::OscDispatcher;
+//! use bevy_osc::OscMethod;
+//!
+//! #[derive(Component)]
+//! struct ExampleEntity;
+//!
+//! #[derive(Bundle)]
+//! #[derive(Component)]
+//! struct ExampleBundle {
+//!     _t: ExampleEntity,
+//!     receiver: OscMethod,
+//! }
+//!
+//! fn spawn(mut commands: Commands) {
+//!     commands.spawn_bundle(TestBundle {
+//!             _t: TestEntity,
+//!             receiver: OscMethod::new("/some/address").expect(""),
+//!         });
+//! }
+//!
+//! fn osc_printer(mut query: Query<&mut OscMethod, (Changed<OscMethod>)>) {
+//!     for mut osc_method in query.iter_mut() {
+//!         match osc_receiver.get_message() {
+//!             Some(message) => println!("Method {} received: {:?}", osc_method.get_address(), msg),
+//!             None => {}
+//!         }
+//!     }
+//! }
+//!
+//! fn main() {
+//!     App::new()
+//!         .add_plugins(MinimalPlugins)
+//!         .insert_resource(OscDispatcher::default())
+//!         .insert_system(osc_printer)
+//!         .run()
+//! }
+//! ```
 
+extern crate rosc;
 
 mod osc_method;
 mod osc_dispatcher;
