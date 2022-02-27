8902b6bc61c74ba4d68d69c6d5e4a6957b9567fc
Lukas Jackowski <l.jackowski@agorate.de>
2022-02-28 00:11:32 +0100

    Add example

---
+++ examples/basic.rs
@@ -0,0 +1,78 @@
+//! A minimal example for how to work with the dispatcher.
+//!
+//! The dispatcher is responsible for
+//!
+
+extern crate bevy_osc;
+
+use bevy::core::FixedTimestep;
+use bevy::prelude::*;
+
+use bevy_osc::OscMethod;
+use bevy_osc::OscDispatcher;
+use rosc::OscMessage;
+use rosc::OscPacket;
+
+#[derive(Component)]
+struct ExampleEntity;
+
+#[derive(Bundle)]
+#[derive(Component)]
+struct ExampleBundle {
+    _t: ExampleEntity,
+    receiver: OscMethod,
+}
+
+/// Startup system that just spawns some entity bundles
+fn startup(mut commands: Commands) {
+    println!("** Startup");
+
+    for i in 0..3 {
+        commands.spawn_bundle(ExampleBundle {
+            _t: ExampleEntity,
+            receiver: OscMethod::new(format!("/entity{}/time", i).as_str()).expect(""),
+        });
+    }
+}
+
+/// System that listens for any `OscMethod` that has changed and then prints out the received OscMessage
+fn print_received_osc_packets(mut query: Query<&mut OscMethod, (Changed<OscMethod>)>) {
+    for mut osc_receiver in query.iter_mut() {
+        let new_msg = osc_receiver.get_message();
+        match new_msg {
+            Some(msg) => {
+                println!("Method {} received: {:?}", osc_receiver.get_address(), msg)
+            }
+            None => {}
+        }
+    }
+}
+
+/// Creates an `OscMessage` and then dispatches it
+fn send_message(mut disp: ResMut<OscDispatcher>, time: Res<Time>, mut query: Query<(&mut OscMethod)>) {
+    let mut new_msg = OscMessage { addr: "/entity*/time".to_string(), args: vec![time.time_since_startup().as_secs_f32().into()] };
+
+    println!("Dispatching: {:?}", new_msg);
+
+    disp.dispatch(OscPacket::Message(new_msg), query);
+}
+
+fn main() {
+    App::new()
+        .add_plugins(MinimalPlugins)
+
+        // Add dispatcher resource
+        .insert_resource(OscDispatcher::default())
+
+        .add_startup_system(startup)
+        .add_system(print_received_osc_packets)
+
+        // Send one OSC Message per second
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(1.0))
+                .with_system(send_message)
+        )
+
+        .run();
+}
