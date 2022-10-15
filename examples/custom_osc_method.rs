0efbac5179b780fc7c15b4163413784845518dd6
DrLuke <Lukas@Jackowski.de>
2022-10-16 00:12:19 +0200

    Add example

---
+++ examples/custom_osc_method.rs
@@ -0,0 +1,47 @@
+//! Add a custom component implementing OscMethod and have it receive messages
+
+extern crate bevy_rosc;
+
+use bevy::prelude::*;
+use rosc::address::OscAddress;
+use rosc::OscMessage;
+
+use bevy_rosc::{BevyRoscPlugin, method_dispatcher_system};
+use bevy_rosc::OscMethod;
+
+#[derive(Component)]
+struct MyOscMethod {
+    osc_address: OscAddress,
+}
+
+impl OscMethod for MyOscMethod {
+    fn get_addresses(&self) -> Vec<OscAddress> {
+        return vec![self.osc_address.clone()];
+    }
+
+    // This method is called when an OSC message was successfully matched with the method
+    fn receive_message(&mut self, osc_message: OscMessage) {
+        println!("MyOscMethod received: {:?}", osc_message)
+    }
+}
+
+fn startup(mut commands: Commands) {
+    println!("** Startup");
+
+    // Spawn entity with custom OSC receiving component
+    commands
+        .spawn()
+        .insert(MyOscMethod{osc_address: OscAddress::new("/test/address".into()).unwrap()});
+}
+
+fn main() {
+    App::new()
+        // Minimal Bevy plugins
+        .add_plugins(MinimalPlugins)
+        // Add the bevy_rosc plugin and have it listen on port 31337
+        .add_plugin(BevyRoscPlugin::new("0.0.0.0:31337").unwrap())
+        // Add dispatcher system for MyOscMethod
+        .add_system(method_dispatcher_system::<MyOscMethod>)
+        .add_startup_system(startup)
+        .run();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! This is a plugin implementing OSC for bevy using `rosc`.
+//! This is a plugin implementing OSC for bevy using [`rosc`].
 //!
 //!
 //!
@@ -36,6 +36,51 @@
 //!         .add_plugin(BevyRoscPlugin::new("0.0.0.0:31337").unwrap())
 //!         .add_startup_system(startup)
 //!         .add_system(print_received_osc_packets)
+//!         .run();
+//! }
+//! ```
+//!
+//! # Advanced usage
+//! There is the option to add custom osc method components.
+//! Your component just has to implement [`OscMethod`] and you need to add a [`method_dispatcher_system`] for it.
+//! Now your component will receive OSC messages at it's address(es).
+//! 
+//! ```no_run
+//! extern crate bevy_rosc;
+//! use bevy::prelude::*;
+//! use bevy_rosc::OscMethod;
+//! use bevy_rosc::{method_dispatcher_system, BevyRoscPlugin};
+//! use rosc::address::OscAddress;
+//! use rosc::OscMessage;
+//! 
+//! #[derive(Component)]
+//! struct MyOscMethod {
+//!     osc_address: OscAddress,
+//! }
+//! 
+//! impl OscMethod for MyOscMethod {
+//!     fn get_addresses(&self) -> Vec<OscAddress> {
+//!         return vec![self.osc_address.clone()];
+//!     }
+//! 
+//!     // This method is called when an OSC message was successfully matched with the method
+//!     fn receive_message(&mut self, osc_message: OscMessage) {
+//!         println!("MyOscMethod received: {:?}", osc_message)
+//!     }
+//! }
+//! 
+//! fn startup(mut commands: Commands) {
+//!     commands.spawn().insert(MyOscMethod {
+//!         osc_address: OscAddress::new("/test/address".into()).unwrap(),
+//!     });
+//! }
+//! 
+//! fn main() {
+//!     App::new()
+//!         .add_plugins(MinimalPlugins)
+//!         .add_plugin(BevyRoscPlugin::new("0.0.0.0:31337").unwrap())
+//!         .add_system(method_dispatcher_system::<MyOscMethod>) // <-- Add dispatcher system for your method
+//!         .add_startup_system(startup)
 //!         .run();
 //! }
 //! ```
